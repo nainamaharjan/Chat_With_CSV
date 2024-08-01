5168cb285eac21f60ac600674a2be36523e1541b
Naina12345 <nainamaharjan99@gmail.com>
2024-08-02 03:14:51 +0545

    Added rows

--- settings.py
+++ settings.py
@@ -18,6 +18,6 @@ EMBEDDINGS_MODEL_NAME = "BAAI/bge-base-en-v1.5"
 LLM_MODEL_NAME = "gpt-3.5-turbo"
 API_ENDPOINT = "http://localhost:8001/query"
 CSV_FILE = get_absolute_path_for_resource_files("Covid Data.csv")
-ROWS_TO_ADD = 1000
+ROWS_TO_ADD = 10000
 KEY = os.environ.get("API_KEY")
 API_KEY = KEY
